221145202d542e2af3a04844f62a0aa38dea17a9
vikanshu <vikanshu.joshi@go-mmt.com>
2025-12-04 16:26:00 +0530

    modified arch

--- app/src/main/java/com/example/practice/detail/HotelDetailScreen.kt
+++ app/src/main/java/com/example/practice/detail/HotelDetailScreen.kt
@@ -1,14 +1,24 @@
 package com.example.practice.detail
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import com.example.practice.detail.base.DetailCards
 
 @Composable
 fun HotelDetailScreen(
-    cards: List<DetailCards>
+    cards: SnapshotStateList<DetailCards>
 ) {
-    cards.forEach { card ->
-        card.UI(modifier = Modifier)
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(items = cards, key = { card ->
+            card.cardId
+        }) { card ->
+            card.UI(modifier = Modifier)
+        }
     }
 }
